a9c63a4953016e0b49d0ed1d1b6587603bce08a0
mohamedameen01 <ameentp64@gmail.com>
2024-01-04 23:10:30 +0530

    BottomBar, CreatePost and FileUploader
    Installed React-Dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import './globals.css';
 import SigninForm from './auth/forms/SigninForm';
 import SignupForm from './auth/forms/SignupForm';
-import Home from './root/pages/Home';
 import AuthLayout from './auth/AuthLayout';
 import RootLayout from './root/RootLayout';
 import { Toaster } from "@/components/ui/toaster"
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './root/pages';
+
 
 const App = () => {
   return (
@@ -19,6 +20,14 @@ const App = () => {
         {/*private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:id' element={<EditPost />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
+          <Route path='/profile/:id/*' element={<Profile />} />
+          <Route path='/update-profile/:id' element={<UpdateProfile />} />
         </Route>
       </Routes>
 
--- src/components/shared/LeftSideBar.tsx
+++ src/components/shared/LeftSideBar.tsx
@@ -55,10 +55,13 @@ function LeftSideBar() {
             const isActive = pathname === link.route;
 
             return (
-              <li key={link.label} className="leftsidebar-link group" >
+              <li 
+                key={link.label} 
+                className={`leftsidebar-link group ${isActive && "bg-primary-500"}`} 
+              >
                 <NavLink
                   to={link.route}
-                  className={`flex gap-4 items-center p-4 ${isActive && "bg-primary-500"}`}
+                  className="flex gap-4 items-center p-4"
                 >
                   <img 
                     src={link.imgURL} 
